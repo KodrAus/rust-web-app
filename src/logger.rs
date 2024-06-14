313221b83baf143171d37f9dee3c1cf8b1a8bb56
Ashley Mannix <kodraus@hey.com>
2024-06-14 10:59:34 +1000

    remove old logger code and comments

--- src/logger.rs
+++ src/logger.rs
@@ -1,19 +1,10 @@
 /*!
 Application logging.
-
-This module wraps a logger to produce line-delimited JSON instead of regular text.
-That makes it a bit nicer to consume through some sidecar or ambient environment
-that collects and surfaces log events.
 */
 
 use std::time::Duration;
 
 use emit::Emitter;
-
-/** The environment variable to read the level filter from. */
-pub const LOG_LEVEL_ENV: &str = "LOG_LEVEL";
-/** The environment variable to read the style info from. */
-pub const LOG_STYLE_ENV: &str = "LOG_STYLE";
 
 /** Initialize the global logger. */
 pub fn init() {
