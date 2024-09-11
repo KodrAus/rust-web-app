324c842609216a32324cec221b1e62392553cdef
Ashley Mannix <kodraus@hey.com>
2024-09-12 07:24:53 +1000

    update emit to latest (#45)

--- src/logger.rs
+++ src/logger.rs
@@ -8,7 +8,15 @@ use emit::Emitter;
 
 /** Initialize the global logger. */
 pub fn init() {
-    let _ = emit::setup().emit_to(emit_term::stdout()).init();
+    let _ = emit::setup()
+        .emit_to(emit_term::stdout())
+        .emit_to(emit_otlp::new()
+            .logs(emit_otlp::logs_grpc_proto("http://localhost:4319"))
+            .traces(emit_otlp::traces_grpc_proto("http://localhost:4319"))
+            .metrics(emit_otlp::metrics_grpc_proto("http://localhost:4319"))
+            .spawn()
+         )
+        .init();
 }
 
 /** Flush the global logger. */
